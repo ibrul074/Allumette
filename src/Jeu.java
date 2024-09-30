51c05dcc93cf84a32f6c8d9afe3a55d0470e2340
Isabelle Brule <ibrul074@gmail.com>
2024-09-30 10:39:36 -0400

    Utilisation boucles do while et message gagnant

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Jeu {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Saisir le joueur 1 : ");
+        System.out.print("Saisir le nom du joueur 1 : ");
         String j1= sc.nextLine();
-        System.out.print("Saisir le joueur 2 : ");
+        System.out.print("Saisir le nom du joueur 2 : ");
         String j2= sc.nextLine();
         int joueur=1;
         int alu=13;
         int retire=0;
 
-        while (alu>0){
+        do{
             System.out.print("*************  ");
             if (joueur==1){
                 System.out.print(j1);
@@ -29,12 +31,10 @@ public class Jeu {
             }
             System.out.println();
             while (retire<1 || retire>3){
-                System.out.print("Saisir le nombre a retirer : ");
-                retire = sc.nextInt();
-                while (retire>alu){
+                do{
                     System.out.print("Saisir le nombre a retirer : ");
                     retire = sc.nextInt();
-                }
+                }while (retire>alu);
                 if (retire==1){
                     alu--;
                 }
@@ -44,19 +44,17 @@ public class Jeu {
                 else if (retire==3){
                     alu-=3;
                 }
-                else {
-                    continue;
-                }
             }
             retire=0;
-        }
+        }while (alu>0);
+
         System.out.print("Le joueur ");
-        if (joueur!=1){
+        if (joueur == 1){
             System.out.print(j1);
         }
         else{
             System.out.print(j2);
         }
-        System.out.print(" a perdu");
+        System.out.print(" a gagner!");
     }
 }
